7d79487a155f22e6b5346e1911ea0941bc6825c2
alexpbrown <alexpbrown9@gmail.com>
2011-02-10 20:48:11 -0500

    Added classes for Card and Deck

---
+++ Card.vb
@@ -0,0 +1,49 @@
+﻿Public Class Card
+    Dim Suits() As String = {"S", "D", "C", "H"}
+    Dim Faces() As String = {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"}
+
+    Private mFace As String
+    Private mSuit As String
+    Protected cValue As Integer
+    Protected sValue As Integer
+
+    Public ReadOnly Property Suit() As String
+        Get
+            Return mSuit
+        End Get
+    End Property
+
+    Public ReadOnly Property Face() As String
+        Get
+            Return mFace
+        End Get
+    End Property
+
+    Public ReadOnly Property CardValue() As Integer
+        Get
+            Return cValue
+        End Get
+    End Property
+
+    Public ReadOnly Property SuitValue() As Integer
+        Get
+            Return sValue
+        End Get
+    End Property
+
+    'Constructor for the class, initializes values for the card
+    Public Sub New(ByVal newSuit As String, ByVal newFaceVal As String)
+        mFace = newFaceVal
+        mSuit = newSuit
+        cValue = Array.IndexOf(Of String)(Faces, mFace)
+        sValue = Array.IndexOf(Of String)(Suits, mSuit)
+    End Sub
+
+
+    'Returns the card as a string (e.g. "S6" six of spades)
+    Public Overrides Function ToString() As String
+        Return mSuit & mFace
+    End Function
+
+    'Implement card jpegs as a sub here
+End Class
---
+++ Deck.vb
@@ -0,0 +1,36 @@
+﻿Public Class Deck
+    Dim r As New Random(Now.Millisecond)
+    Dim mDeck As New List(Of Card)
+    Dim Suits() As String = {"S", "D", "C", "H"}
+    Dim Faces() As String = {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"}
+
+    'constructor that adds one of each card to the deck
+    Public Sub New()
+        For Each suit As String In Suits
+            For Each face As String In Faces.Reverse
+                mDeck.Add(New Card(suit, face))
+            Next
+        Next
+    End Sub
+
+    Public Sub Shuffle()
+        Dim card As Card
+        Dim cardNum As Integer
+        For i As Integer = 0 To 52
+            cardNum = r.Next(0, 51) Mod mDeck.Count 'TEST THIS FOR RANDOMNESS
+            card = mDeck(cardNum)
+            mDeck.RemoveAt(cardNum)
+            mDeck.Add(card)
+        Next
+    End Sub
+
+    Public ReadOnly Property DeckState() As String
+        Get
+            Dim d As String = ""
+            For Each c As Card In mDeck
+                d &= c.ToString
+            Next
+            Return d
+        End Get
+    End Property
+End Class
