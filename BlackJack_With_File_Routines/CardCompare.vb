68eb28370b8431fb9200afed765abae98d01973c
Alex Brown <alexpbrown9@gmail.com>
2011-02-11 14:04:34 -0500

    Fixing directory structure

---
+++ BlackJack_With_File_Routines/CardCompare.vb
@@ -0,0 +1,10 @@
+﻿'Public Class CardCompare
+'    Implements IComparer(Of PlayingCard)
+
+'    Public Function Compare(ByVal x As PlayingCard, ByVal y As PlayingCard) As Integer Implements System.Collections.Generic.IComparer(Of PlayingCard).Compare
+'        Dim order() As String = {"A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"}
+'        Dim xOrdered = Array.IndexOf(Of String)(order, CStr(x.ToString)(1))
+'        Dim yOrdered = Array.IndexOf(Of String)(order, CStr(y.ToString)(1))
+'        Return xOrdered.CompareTo(yOrdered)
+'    End Function
+'End Class
