4d44d2905a3a3d7a89ce04e49996f4e13b39fd77
Alex Brown <alexpbrown9@gmail.com>
2011-02-12 23:25:15 -0500

    Added player class

---
+++ BlackJack_With_File_Routines/Player.vb
@@ -0,0 +1,40 @@
+﻿Public Class Player
+
+    Private gamesWon As Integer     ' number of times the player has won
+    Private gamesPlayed As Integer  ' number of games played
+    Private totalWinnings As Decimal    'total player winnings
+
+    Public Property Won() As Integer
+        Get
+            Return gamesWon
+        End Get
+        Set(ByVal value As Integer)
+            gamesWon = value
+        End Set
+    End Property
+
+    Public Property Played() As Integer
+        Get
+            Return gamesPlayed
+        End Get
+        Set(ByVal value As Integer)
+            gamesPlayed = value
+        End Set
+    End Property
+
+    Public Property Winnings() As Decimal
+        Get
+            Return totalWinnings
+        End Get
+        Set(ByVal value As Decimal)
+            totalWinnings = value
+        End Set
+    End Property
+
+    Public Sub New()
+        gamesPlayed = 0
+        gamesWon = 0
+        totalWinnings = 0
+    End Sub
+
+End Class
